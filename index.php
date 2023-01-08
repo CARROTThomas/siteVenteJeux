6102df7e291ce45fb60d744fc9d6ab46acb2a70f
CARROTThomas <thomas.carrot38240@gmail.com>
2023-01-08 17:02:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once ('librairies/tools.php');
+require_once ('pdo.php');
+
+/*
+render("base", [
+    "pageTitle"=>"accueil du Site"
+]);
+*/
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test</title>
+</head>
+<body>
+<h1>test</h1>
+</body>
+</html>
---
+++ templates/base.html.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+    <link rel="website icon" type="png" href="img/controller">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>
+<!---->
+
+
+
+<div class="container">
+    <h1>Test</h1>
+    <!-- Templates -->
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<script src="../JS/main.js"></script>
+</body>
+</html>
